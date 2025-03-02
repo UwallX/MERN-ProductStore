2f83ab6a1b270e08fb0c984ef559ac7b71604a33
Uallace <uallace.acad@gmail.com>
2025-03-02 10:38:14 -0300

    feat: add CreateProduct page

--- client/src/pages/CreatePage/CreatePage.jsx
+++ client/src/pages/CreatePage/CreatePage.jsx
@@ -1,6 +1,69 @@
+import { Container, Heading, VStack, Box, Input, Button } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { useColorModeValue } from "@/components/ui/color-mode";
+import { useProductStore } from "../../store/product";
+import toast from 'react-hot-toast';
+
 const CreatePage = () => {
+  const { register, handleSubmit, reset } = useForm({
+    defaultValues: {
+      name: null,
+      price: null,
+      image: null
+    }
+  });
+
+  const { createProduct } = useProductStore();
+
+  const onSubmitData = async (data) => {
+    const { success, message } = await createProduct(data);
+    if(success) {
+      toast.success(message);
+      reset();
+    } else {
+      toast.error(message);
+    }
+  }
+
   return (
-    <div>CreatePage</div>
+    <Container maxW={"container.sm"}>
+      <VStack padding={8}>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
+          Criar novo produto
+        </Heading>
+
+        <Box
+          w={"full"} bg={useColorModeValue("white", "gray.900")}
+          p={6} rounded={"lg"} shadow={"md"}
+        >
+          <VStack padding={4}>
+            <Input
+              {...register('name')}
+              placeholder='Nome do Produto'
+              name='name'
+              type='text'
+              borderWidth="1px"
+            />
+            <Input
+              {...register('price')}
+              placeholder='Preço'
+              name='price'
+              type='number'
+            />
+            <Input
+              {...register('image')}
+              placeholder='Imagem'
+              name='image'
+              type='text'
+            />
+
+            <Button width='full' colorScheme='blue' onClick={handleSubmit(onSubmitData)}>
+              Adicionar produto
+            </Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
   )
 }
 
--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import { connectDb } from './config/db.js';
 import routes from './routes/index.js';
+import cors from 'cors';
 
 const PORT = process.env.SERVER_PORT || 5000;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(routes);
 
 app.listen(PORT, () => {
